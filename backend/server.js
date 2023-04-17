df02835412423d9e60530bdab224b5acf2e04ed4
Jerom3T <jerom3tonnelier@gmail.com>
2023-04-17 23:27:05 +0200

    Creation Backend

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch((error) => console.log('Error connecting to MongoDB:', error));
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
