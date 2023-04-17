24a68182c84343e153533d7c2a25a228feb4b0bf
Jerom3T <jerom3tonnelier@gmail.com>
2023-04-17 23:36:56 +0200

    Server

--- backend/server.js
+++ backend/server.js
@@ -18,5 +18,5 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`); 
 });
